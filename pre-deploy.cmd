cb50ff1bfe4bdab43b3725c35a5c0e03e8cff21c
Andrey Kovalenko <ak@taucode.com>
2022-08-12 14:52:52 +0300

    Trivia.

--- pre-deploy.cmd
+++ pre-deploy.cmd
@@ -6,7 +6,7 @@ dotnet clean --configuration Release
 dotnet build --configuration Debug
 dotnet build --configuration Release
 
-dotnet test -c Debug .\tests\TauCode.AppComposition.Mq.Tests\TauCode.AppComposition.Mq.Tests.csproj
-dotnet test -c Release .\tests\TauCode.AppComposition.Mq.Tests\TauCode.AppComposition.Mq.Tests.csproj
+dotnet test -c Debug .\test\TauCode.AppComposition.Mq.Tests\TauCode.AppComposition.Mq.Tests.csproj
+dotnet test -c Release .\test\TauCode.AppComposition.Mq.Tests\TauCode.AppComposition.Mq.Tests.csproj
 
 nuget pack nuget\TauCode.AppComposition.Mq.nuspec
